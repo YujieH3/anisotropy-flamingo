406f4892d01a75ac8418885624b0fe9a15762220
YujieH3 <yujie.jay.he@outlook.com>
2024-09-20 20:28:16 +0200

    Updated .gitignore, removed notebooks

--- scripts/__fetch_linked.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-scp cosma8:/cosma8/data/do012/dc-he4/halo_properties_in*.hdf5 /data1/yujiehe/data/
-
--- scripts/test.py
+++
@@ -1,5 +0,0 @@
-from tqdm import tqdm
-import time
-for i1 in range(5):
-    time.sleep(1)
-print('done')
