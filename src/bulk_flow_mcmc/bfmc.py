Debugged MC fit-all to use correct normalisation

src/bulk_flow_mcmc/bfmc.py
@@ -77,7 +77,7 @@ def log_likelihood(theta, X, Y, z_obs, phi_lc, theta_lc, yname, xname):
     # Calculate the redshift
     angle = cf.angular_separation(phi_lc, theta_lc, vlon, vlat) * np.pi/180
     
-    # Correct the bulk flow accordingly: z_bf = z_obs + ubf * (1 + z_bf) * np.cos(angle) / C
+    # Non-relativistic correction 
     z_bf = (z_obs + ubf * np.cos(angle) / C) / (1 - ubf * np.cos(angle) / C) # Non-relativistic correction
 
     # The relativistic correction

tools/constants.py
@@ -190,7 +190,7 @@ def get_const(relation, key, array_const=ARRAY_CONST):
 
 LARGE_RANGE = {
     'LX-T': {
-        'logA_min': np.log10(0.1), 'logA_max': np.log10(2.0),
+        'logA_min': np.log10(0.1), 'logA_max': np.log10(3.0),
         'B_min'   : 1,             'B_max'   : 3.5,
         'scat_min': 0.09,          'scat_max': 1,
     },
@@ -223,7 +223,7 @@ LARGE_RANGE = {
 
 MID_RANGE = { # round 5 sigma range to .5. Round up upper range and round down lower range
     'LX-T': {
-        'logA_min': np.log10(1.0), 'logA_max': np.log10(2.0),
+        'logA_min': np.log10(1.0), 'logA_max': np.log10(3.0),
         'B_min'   : 1.5,           'B_max'   : 3.5,
         'scat_min': 0.05,          'scat_max': 1,
     },
@@ -233,7 +233,7 @@ MID_RANGE = { # round 5 sigma range to .5. Round up upper range and round down l
         'scat_min': 0.05,          'scat_max': 1,
     },
     'M-T': {
-        'logA_min': np.log10(0.5), 'logA_max': np.log10(1.5),
+        'logA_min': np.log10(0.5), 'logA_max': np.log10(2.0),
         'B_min'   : 1.5,           'B_max'   : 2.5,
         'scat_min': 0.05,          'scat_max': 1,
     },